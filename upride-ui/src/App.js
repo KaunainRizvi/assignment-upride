778a3b3c13cf1266406865093b3a85cd9b00d3ad
KaunainRizvi <krizvibusiness.com>
2023-09-19 00:20:22 +0530

    ui completed static

---
+++ upride-ui/src/App.js
@@ -0,0 +1,355 @@
+import React from "react";
+import "./index.css";
+
+export const App = () => {
+  return (
+    <div className="macbook-air">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="rectangle" />
+            <div className="div" />
+            <img className="ellipse" alt="Ellipse" src="https://c.animaapp.com/Zz5CTPE5/img/ellipse-4@2x.png" />
+            <div className="text-wrapper">Hello Lokesh!</div>
+            <img
+              className="emoji-waving-hand"
+              alt="Emoji waving hand"
+              src="https://c.animaapp.com/Zz5CTPE5/img/---emoji--waving-hand-@2x.png"
+            />
+            <div className="rectangle-2" />
+            <div className="group">
+              <div className="overlap-2">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-3">
+                    <div className="div-wrapper">
+                      <div className="overlap-group-2">
+                        <div className="text-wrapper-2">Rajarajeshwari Nagar</div>
+                      </div>
+                    </div>
+                    <img className="location" alt="Location" src="https://c.animaapp.com/Zz5CTPE5/img/location-1.svg" />
+                    <div className="text-wrapper-3">Branch 2</div>
+                    <div className="text-wrapper-4">Branch 3</div>
+                  </div>
+                </div>
+                <img className="line" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-17.svg" />
+                <div className="rectangle-3" />
+                <div className="rectangle-4" />
+                <img
+                  className="caret-down"
+                  alt="Caret down"
+                  src="https://c.animaapp.com/Zz5CTPE5/img/caret-down-2.svg"
+                />
+              </div>
+            </div>
+            <div className="group-2" />
+            <div className="rectangle-5" />
+            <img className="vector" alt="Vector" src="https://c.animaapp.com/Zz5CTPE5/img/vector.svg" />
+            <img
+              className="my-earning-logo"
+              alt="My earning logo"
+              src="https://c.animaapp.com/Zz5CTPE5/img/my-earning-logo@2x.png"
+            />
+            <div className="group-3">
+              <div className="overlap-4">
+                <img
+                  className="logoupride"
+                  alt="Logoupride"
+                  src="https://c.animaapp.com/Zz5CTPE5/img/logoupride2-1@2x.png"
+                />
+                <img
+                  className="untitled-design"
+                  alt="Untitled design"
+                  src="https://c.animaapp.com/Zz5CTPE5/img/untitled-design-1@2x.png"
+                />
+              </div>
+            </div>
+            <img className="img" alt="Group" src="https://c.animaapp.com/Zz5CTPE5/img/group-3@2x.png" />
+            <div className="text-wrapper-5">Home</div>
+            <div className="text-wrapper-6">My Earnings</div>
+            <div className="text-wrapper-7">My Assets</div>
+            <div className="text-wrapper-8">My Services</div>
+            <div className="group-4">
+              <div className="overlap-5">
+                <img className="group-5" alt="Group" src="https://c.animaapp.com/Zz5CTPE5/img/group-2@2x.png" />
+                <div className="text-wrapper-9">Search bookings</div>
+              </div>
+            </div>
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <img className="group-6" alt="Group" src="https://c.animaapp.com/Zz5CTPE5/img/group-1@2x.png" />
+            <div className="text-wrapper-10">New Booking</div>
+            <img className="add" alt="Add" src="https://c.animaapp.com/Zz5CTPE5/img/add-2-1.svg" />
+          </div>
+          <div className="overlap-6">
+            <div className="text-wrapper-11">View Bookings</div>
+            <img className="group-7" alt="Group" src="https://c.animaapp.com/Zz5CTPE5/img/group@2x.png" />
+          </div>
+          <div className="group-8">
+            <div className="overlap-7">
+              <div className="group-9">
+                <div className="text-wrapper-12">Active</div>
+                <div className="text-wrapper-13">Completed</div>
+                <div className="text-wrapper-14">Cancelled</div>
+                <img className="line-2" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-18.svg" />
+              </div>
+              <img className="line-3" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-19.svg" />
+            </div>
+          </div>
+          <div className="group-wrapper">
+            <div className="group-10">
+              <div className="overlap-8">
+                <img className="line-4" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-20.svg" />
+                <img className="line-5" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-6" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-7" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-8" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-24.svg" />
+                <img className="line-9" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-10" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-11" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <img className="line-12" alt="Line" src="https://c.animaapp.com/Zz5CTPE5/img/line-21.svg" />
+                <div className="group-11">
+                  <div className="group-12">
+                    <div className="overlap-9">
+                      <div className="rectangle-8" />
+                      <div className="group-13">
+                        <div className="group-14">
+                          <img
+                            className="rectangle-9"
+                            alt="Rectangle"
+                            src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-144@2x.png"
+                          />
+                          <div className="steve-bowen">Ayush Sinha</div>
+                          <p className="drop-off">
+                            <span className="span"> O</span>
+                            <span className="span">ct 26, 2022</span>
+                          </p>
+                          <div className="element-swift-village">4W - Compact SUV</div>
+                          <div className="group-15">
+                            <div className="group-16">
+                              <div className="group-17">
+                                <div className="apple-pay-wrapper">
+                                  <div className="apple-pay">Online Payment</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-18">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="overlap-group-3">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-20">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="apple-pay-wrapper">
+                                <div className="apple-pay-2">Offline Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-21">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="overlap-group-3">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-22">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="apple-pay-wrapper">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-23">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="overlap-group-3">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-24">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-19">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="apple-pay-wrapper">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-25">
+                        <div className="group-14">
+                          <img
+                            className="rectangle-10"
+                            alt="Rectangle"
+                            src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                          />
+                          <div className="steve-bowen-2">Ayush Sinha</div>
+                          <p className="p">
+                            <span className="span"> O</span>
+                            <span className="span">ct 26, 2022</span>
+                          </p>
+                          <div className="element-swift-village-2">4W - Compact SUV</div>
+                          <div className="group-19">
+                            <div className="group-16">
+                              <div className="group-17">
+                                <div className="overlap-group-3">
+                                  <div className="apple-pay">Online Payment</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-26">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-27">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="apple-pay-wrapper">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="group-28">
+                        <img
+                          className="rectangle-10"
+                          alt="Rectangle"
+                          src="https://c.animaapp.com/Zz5CTPE5/img/rectangle-60@2x.png"
+                        />
+                        <div className="steve-bowen-2">Ayush Sinha</div>
+                        <p className="p">
+                          <span className="span"> O</span>
+                          <span className="span">ct 26, 2022</span>
+                        </p>
+                        <div className="element-swift-village-2">4W - Compact SUV</div>
+                        <div className="group-27">
+                          <div className="group-16">
+                            <div className="group-17">
+                              <div className="apple-pay-wrapper">
+                                <div className="apple-pay">Online Payment</div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="overlap-10">
+                      <div className="text-wrapper-15">Name</div>
+                      <div className="text-wrapper-16">Payment Mode</div>
+                      <div className="text-wrapper-17">Package Details</div>
+                      <div className="text-wrapper-18">Date</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-11" />
+        </div>
+      </div>
+    </div>
+  );
+};
